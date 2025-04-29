2cfb6541564e63b20ea5a3acf56f52adadac4c11
Hasan Malik <mhasanamalik03@gmail.com>
2025-04-29 08:35:01 +0500

    Changes

--- src/components/RotatatingText.jsx
+++ src/components/RotatatingText.jsx
@@ -1,10 +1,14 @@
 // Component for adding the rotating text when hovered over buttons, paddles etc
 import React from 'react'
+import { Html } from '@react-three/drei'
+import gsap from 'gsap'
 
 function RotatatingText() {
   return (
    <>
-    
+    <Html>
+      
+    </Html>
    </>
   )
 }
