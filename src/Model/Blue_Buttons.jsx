bb5731ee534a81e32a92f1370327a07a156a0d86
Hasan Malik <mhasanmalik03@gmail.com>
2025-07-04 23:41:58 +0500

    changes

--- src/Model/Blue_Buttons.jsx
+++ src/Model/Blue_Buttons.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 function Blue_Buttons({
     geometry_1, material_1, position_1,
@@ -13,44 +12,44 @@ function Blue_Buttons({
         name="blue_buttons_1"
         castShadow
         receiveShadow
-        geometry_1={geometry_1}
-        material_1={material_1}
-        position_1={position_1}
+        geometry={geometry_1}
+        material={material_1}
+        position={position_1}
       />
 
       <mesh
         name="blue_buttons_2"
         castShadow
         receiveShadow
-        geometry_2={geometry_2}
-        material_2={material_2}
-        position_2={position_2}
+        geometry={geometry_2}
+        material={material_2}
+        position={position_2}
       />
       <mesh
         name="blue_buttons_3"
         castShadow
         receiveShadow
-        geometry_3={geometry_3}
-        material_3={material_3}
-        position_3={position_3}
+        geometry={geometry_3}
+        material={material_3}
+        position={position_3}
       />
 
       <mesh
         name="blue_buttons_4"
         castShadow
         receiveShadow
-        geometry_4={geometry_4}
-        material_4={material_4}
-        position_4={position_4}
+        geometry={geometry_4}
+        material={material_4}
+        position={position_4}
       />
 
       <mesh
         name="blue_buttons_5"
         castShadow
         receiveShadow
-        geometry_5={geometry_5}
-        material_5={material_5}
-        position_5={position_5}
+        geometry={geometry_5}
+        material={material_5}
+        position={position_5}
       />
 
     
