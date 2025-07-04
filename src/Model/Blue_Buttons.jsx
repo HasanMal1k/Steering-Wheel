4229b6216bed1ea1b72dd8e573f3014c4f30e5d9
Hasan Malik <mhasanmalik03@gmail.com>
2025-07-05 01:04:43 +0500

    changes

--- src/Model/Blue_Buttons.jsx
+++ src/Model/Blue_Buttons.jsx
@@ -1,3 +1,6 @@
+import React, { useEffect, useMemo, useState } from "react"
+import { useConfigurationStore } from "../ConfigurationStore"
+
 
 function Blue_Buttons({
     geometry_1, material_1, position_1,
@@ -5,6 +8,55 @@ function Blue_Buttons({
     geometry_3, material_3, position_3,
     geometry_4, material_4, position_4,
     geometry_5, material_5, position_5}) {
+    
+    const activeComponent = useConfigurationStore(state => state.activeComponent)
+
+    const clonedMaterial_1 = useMemo(() => material_1.clone(), [])
+    const clonedMaterial_2 = useMemo(() => material_2.clone(), [])
+    const clonedMaterial_3 = useMemo(() => material_3.clone(), [])
+    const clonedMaterial_4 = useMemo(() => material_4.clone(), [])
+    const clonedMaterial_5 = useMemo(() => material_5.clone(), [])
+    
+    useEffect(()=>{
+
+        if(activeComponent){
+
+            clonedMaterial_1.transparent = true
+            clonedMaterial_1.opacity = 0.3
+
+            clonedMaterial_2.transparent = true
+            clonedMaterial_2.opacity = 0.3
+
+            clonedMaterial_3.transparent = true
+            clonedMaterial_3.opacity = 0.3
+
+            clonedMaterial_4.transparent = true
+            clonedMaterial_4.opacity = 0.3
+
+            clonedMaterial_5.transparent = true
+            clonedMaterial_5.opacity = 0.3
+        }
+        else{
+            clonedMaterial_1.transparent = false
+            clonedMaterial_1.opacity = 1
+
+            clonedMaterial_2.transparent = false
+            clonedMaterial_2.opacity = 1
+
+            clonedMaterial_3.transparent = false
+            clonedMaterial_3.opacity = 1
+
+            clonedMaterial_4.transparent = false
+            clonedMaterial_4.opacity = 1
+
+            clonedMaterial_5.transparent = false
+            clonedMaterial_5.opacity = 1
+        }
+
+    }, [activeComponent])
+
+
+
   return (
     <>
     
@@ -13,8 +65,9 @@ function Blue_Buttons({
         castShadow
         receiveShadow
         geometry={geometry_1}
-        material={material_1}
+        material={clonedMaterial_1}
         position={position_1}
+        
       />
 
       <mesh
@@ -22,7 +75,7 @@ function Blue_Buttons({
         castShadow
         receiveShadow
         geometry={geometry_2}
-        material={material_2}
+        material={clonedMaterial_2}
         position={position_2}
       />
       <mesh
@@ -30,7 +83,7 @@ function Blue_Buttons({
         castShadow
         receiveShadow
         geometry={geometry_3}
-        material={material_3}
+        material={clonedMaterial_3}
         position={position_3}
       />
 
@@ -39,7 +92,7 @@ function Blue_Buttons({
         castShadow
         receiveShadow
         geometry={geometry_4}
-        material={material_4}
+        material={clonedMaterial_4}
         position={position_4}
       />
 
@@ -48,7 +101,7 @@ function Blue_Buttons({
         castShadow
         receiveShadow
         geometry={geometry_5}
-        material={material_5}
+        material={clonedMaterial_5}
         position={position_5}
       />
     </>
